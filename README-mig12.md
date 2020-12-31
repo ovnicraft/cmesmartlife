cd773155c7a3e35a9989b4ad620149085671f37f
Cristian Salamea <salamea@vauxoo.com>
2020-12-31 11:37:32 -0500

    not load views

---
+++ README-mig12.md
@@ -0,0 +1,3 @@
+1. execute sql from here
+2. execute sql from l10n-ec/migration
+3. install openupgradelib
---
+++ upgrade_12.sql
@@ -0,0 +1,4 @@
+update ir_module_module set state = 'uninstalled' where name = 'pos_invoice_auto_check';
+update ir_module_module set state = 'uninstalled' where name = 'pos_combo_product';
+update ir_module_module set state = 'uninstalled' where name = 'pos_payment_reference';
+update ir_module_module set state = 'uninstalled' where name = 'l10n_ec_pos';
