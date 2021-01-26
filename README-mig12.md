1ce40be40f92cc9d12b6fa3665e31a45ad7943e9
Cristian Salamea <salamea@vauxoo.com>
2021-01-26 01:12:51 -0500

    [MIG] fix migrations

--- README-mig12.md
+++ README-mig12.md
@@ -1,3 +1,7 @@
 1. execute sql from here
 2. execute sql from l10n-ec/migration
 3. install openupgradelib
+4. uninstall modules = env["ir.module.module"].search([('name','in', ['stock_picking_internal','account_financial_report_qweb', 'event_management', 'account_financial_report_date_range','account_group', 'contract_sale_ext']),('state','in',['installed','to upgrade', ])])
+
+migrar foreign_trade, landed_costs_ext
+
--- upgrade_12.sql
+++ upgrade_12.sql
@@ -2,3 +2,6 @@ update ir_module_module set state = 'uninstalled' where name = 'pos_invoice_auto
 update ir_module_module set state = 'uninstalled' where name = 'pos_combo_product';
 update ir_module_module set state = 'uninstalled' where name = 'pos_payment_reference';
 update ir_module_module set state = 'uninstalled' where name = 'l10n_ec_pos';
+update ir_module_module set state = 'uninstalled' where name = 'pos_operations';
+update ir_module_module set state = 'uninstalled' where name = 'pos_close_ui';
+update ir_module_module set state = 'uninstalled' where name = 'invoice_taxexcent';
